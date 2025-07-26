e6f27bec1d4332ffccd5de52bcb285b204983a07
Jayasimha2006 <2420080035@klh.edu.in>
2025-07-26 09:20:41 +0530

    Add files via upload

---
+++ IF_else.java
@@ -0,0 +1,12 @@
+package practical1;
+
+public class IF_else {
+	public static void main(String[] args) {
+		int num = 7;
+		if(num%2==0) {
+			System.out.println("Even number");
+		} else {
+			System.out.println("odd number");
+		}
+	}
+}
---
+++ logicaloperators.java
@@ -0,0 +1,11 @@
+package practical1;
+
+public class logicaloperators {
+	public static void main(String[] args) {
+        int a = 10, b = 5, c=20;
+        System.out.println((a > b) && (a < c));	//true
+        System.out.println((a > b) && (a < c));	//true
+        System.out.println(!(a < c));			//false
+	}
+}
+
